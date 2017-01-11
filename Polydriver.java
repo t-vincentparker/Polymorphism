a5363689a168edfc45208d9c6cab4b197edd4ce8
Vincent Parker <[]>
2017-01-10 16:14:55 -0800

    Added Polydriver and edits to Task class

---
+++ Polydriver.java
@@ -0,0 +1,22 @@
+/* Polydriver.java 		COMP 1231 		Vincent Parker
+ * 
+ * A class to demonstrate the features of Task objects represented
+ * by Task.java
+ * 
+ */
+
+public class Polydriver 
+{
+
+	public static void main(String[] args)
+	{
+	
+		Task Task1 = new Task( "A1", "Update accounts.");
+		
+		System.out.println(Task1.getPriority());
+		
+		
+
+	}
+
+}
